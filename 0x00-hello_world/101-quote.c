024c5ef9d0992a3e84dc5f92bb6e0903534aea4c
Kimani72 <bkimani594@gmail.com>
2023-06-15 16:35:06 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
 #include <unistd.h>
+
 /**
- * main - A program that prints a line 
+ * main - A program that prints a line
  * Return: 1 (Success)
  */
 int main(void)
 {
-write(2,
-"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 return (1);
 }
+
