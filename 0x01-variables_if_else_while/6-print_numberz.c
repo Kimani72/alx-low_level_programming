0648cf064070ef904e5a0d78b860010c433b149d
Kimani72 <bkimani594@gmail.com>
2023-06-18 00:29:22 -0700

    0-positive_or_negative.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - Prints all single digit numbers of base 10 starting from 0. 
+ * Return 0 (Success)
+ */
+int main (void)
+{
+	int i;
+	for (i = 48; i < 58; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - Prints the lowercase alphabet in reverse.
+ * Return 0 (Success)
+ */
+int main(void)
+{
+	char ch;
+	for (ch = 'z'; ch >= 'a'; ch--)
+	{
+		putchar(ch);
+	}
+	putchar('\n');
+	return (0);
+}
