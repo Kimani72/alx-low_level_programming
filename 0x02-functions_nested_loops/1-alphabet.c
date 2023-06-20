77032ff87f939842d949823335f2ee8fd3ac056d
Kimani72 <bkimani594@gmail.com>
2023-06-20 07:21:38 -0700

    main

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the alphabet in lower case.
+ *
+ * Return: 0 (Success)
+ */
+
+int main(void)
+{
+	printf("abcdefghijklmnopqrstuvwxyz\n");
+	return (0);
+}
