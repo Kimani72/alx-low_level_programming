26f5ced398e2ea53b97da8c0f673748f115010d1
Kimani72 <bkimani594@gmail.com>
2023-06-21 14:17:57 -0700

    3-islower.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -2,18 +2,20 @@
 
 /**
  * print_alphabet_x10 - Prints the alphabet 10 times.
+ * Return: void
  */
-
 void print_alphabet_x10(void)
 {
-	char c;
-	int i = 0;
+	char ch;
+	int i;
 
-	while (i <= 9)
+	i = 0;
+
+	while (i < 10)
 	{
-		for (c = 'a'; c <= 'z'; c++)
+		for (ch = 'a'; ch <= 'z'; ch++)
 	{
-		_putchar(c);
+		_putchar(ch);
 	}
 	_putchar('\n');
 	i++;
