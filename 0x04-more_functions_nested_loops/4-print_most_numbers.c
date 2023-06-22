5bd9dac4a351c2e743cebb42ce34026aeafc562d
Kimani72 <bkimani594@gmail.com>
2023-06-22 15:21:54 -0700

    print_most_numbers.c

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_most_numbers - prints 01356789
+ * Return: void
+ */
+
+void print_most_numbers(void)
+{
+	char c;
+
+	for (c = '0'; c <= '9'; c++)
+	{
+		if (! (c == '2' || c == '4'))
+			putchar(c);
+	}
+	putchar('\n');
+}
